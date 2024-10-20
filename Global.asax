2ee28f2008676326da0b4b863a56a15e17e3e2e9
Ashwag Mohsen <79316100+ashwagMohsen@users.noreply.github.com>
2024-10-20 23:36:36 +0300

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sagenoti1.MvcApplication" Language="C#" %>
---
+++ Startup1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(sagenoti1.Startup1))]
+
+namespace sagenoti1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
