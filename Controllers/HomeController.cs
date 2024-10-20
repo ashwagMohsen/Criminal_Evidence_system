f7ed74fb2a0755a5c009ce799181eaab1f0de08e
Ashwag Mohsen <79316100+ashwagMohsen@users.noreply.github.com>
2024-10-21 01:56:45 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sagenoti1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Supervisor()
+        {
+            return View();
+        }
+        public ActionResult Headmaster()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+}
