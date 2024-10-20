3790c620306c22939a67e28c1448425761b80ca2
Ashwag Mohsen <79316100+ashwagMohsen@users.noreply.github.com>
2024-10-21 02:03:47 +0300

    Add files via upload

---
+++ Scripts/notification.js
@@ -0,0 +1,30 @@
+﻿
+
+/////*const { notification } = require("modernizr");
+
+
+////$(function () {
+//// Declare a proxy to reference the hub
+//var notificationHub = $.connection.notificationHub;
+
+//// Create a function that the hub can call toupdate the notification counter and display a bell icon
+
+//notificationHub.client.updateNotificationCounter = function (count) {
+
+//    // Update the notification counter
+//    $('.notification-counter').text(count);
+//    // Show the bell icon
+//    $('.bell-icon').removeClass('hidden');
+//};
+
+//// Start the connection
+//$.connection.hub.start().done(function () {
+//    console.log('SignalR connected');
+//});
+
+///*});*/
+//function k() {
+//    notificationHub.server.sendMessage(1);
+
+
+//}
