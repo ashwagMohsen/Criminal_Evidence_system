279b91e6551017aebfa89341e86a9e92c405a091
Ashwag Mohsen <79316100+ashwagMohsen@users.noreply.github.com>
2024-10-21 01:59:13 +0300

    Add files via upload

---
+++ Models/NotificationHub.cs
@@ -0,0 +1,33 @@
+﻿//using Microsoft.AspNet.SignalR;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace sagenoti1.Hubs
+//{
+//    public class NotificationHub: Hub
+//    {
+     
+//        //    public void sendMessage()
+//        //{
+//        //    // Get the notification count
+//        //    int notificationCount = GetNotificationCount();
+
+//        //    // Get a referenceto the NotificationHub
+//        //    //IHubContext notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+//        //    Clients.All.updateNotificationCounter(notificationCount);
+//        //    // Call the updateNotificationCounter method on all clients
+//        //    //notificationHub.Clients.All.updateNotificationCounter(notificationCount);
+//        //}
+
+//        //private int GetNotificationCount()
+//        //{
+//        //    // Get the notification count from your data source
+//        //    // For example, you could query a database table or call a Web API endpoint
+
+//        //    return 10; // Return a hardcoded value for demonstration purposes
+//        //}
+//    }
+
+//}
---
+++ Models/NotificationService.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNet.SignalR;
+//using sagenoti1.Models;
+//using sagenoti1.Hubs;
+
+namespace sagenoti1.Models
+{
+    public class NotificationHub : Hub
+    {
+        public void sendMessage(int id)
+        {
+            // Get the notification count
+            //int notificationCount = GetNotificationCount();
+
+            // Get a referenceto the NotificationHub
+            //int c = db.reports.where(async =? async.activate == true && async.confirm == false).tolist().count();
+            IHubContext notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+
+            // Call the updateNotificationCounter method on all clients
+            notificationHub.Clients.All.updateNotificationCounter(id);
+        }
+
+        //private int GetNotificationCount()
+        //{
+        //    // Get the notification count from your data source
+        //    // For example, you could query a database table or call a Web API endpoint
+
+        //    return 10; // Return a hardcoded value for demonstration purposes
+        //}
+    }
+}
